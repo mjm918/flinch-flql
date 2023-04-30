28dda3267265c5abc43825a7555cb47b5c8f4165
julfikar <md.julfikar.mahmud@gmail.com>
2023-04-30 12:12:51 +0800

    - renamed crate to `flinch_dql` as `dql` is already exists

--- src/lib.rs
+++ src/lib.rs
@@ -2,331 +2,328 @@
 extern crate pest_derive;
 extern crate pest;
 
-mod flinch_dql {
-    use anyhow::{Result, anyhow, Error};
-    use pest::Parser;
-    use serde_json::Value;
-    use pest::iterators::{Pair, Pairs};
+use anyhow::{Result, anyhow, Error};
+use pest::Parser;
+use serde_json::Value;
+use pest::iterators::{Pair, Pairs};
 
-    #[derive(Parser)]
-    #[grammar = "./dql.pest"]
-    struct DqlParser;
+#[derive(Parser)]
+#[grammar = "./dql.pest"]
+struct DqlParser;
 
-    #[derive(PartialEq, Debug, Clone)]
-    pub enum Direction {
-        Asc,
-        Desc
-    }
+#[derive(PartialEq, Debug, Clone)]
+pub enum Direction {
+    Asc,
+    Desc
+}
 
-    #[derive(PartialEq, Debug, Clone, Default)]
-    pub struct  OffsetLimit {
-        offset: Option<u64>,
-        limit: Option<u64>
-    }
+#[derive(PartialEq, Debug, Clone, Default)]
+pub struct  OffsetLimit {
+    offset: Option<u64>,
+    limit: Option<u64>
+}
 
-    #[derive(PartialEq, Debug, Clone, Default)]
-    pub struct SortDirection {
-        field: Option<String>,
-        direction: Option<Direction>
-    }
+#[derive(PartialEq, Debug, Clone, Default)]
+pub struct SortDirection {
+    field: Option<String>,
+    direction: Option<Direction>
+}
 
-    #[derive(PartialEq, Debug, Clone, Default)]
-    pub struct Clause {
-        and: Option<Value>,
-        or: Option<Value>
-    }
+#[derive(PartialEq, Debug, Clone, Default)]
+pub struct Clause {
+    and: Option<Value>,
+    or: Option<Value>
+}
 
-    #[derive(PartialEq, Debug, Clone)]
-    pub enum Dql {
-        Create(String, Value),
-        Open(String),
-        Drop(String),
-        Len(String),
-        Upsert(String,Value,Option<Clause>),
-        Put(String,String,Value),
-        Exists(String,String),
-        Search(String,String,Option<SortDirection>,Option<OffsetLimit>),
-        GetIndex(String,String),
-        GetWithoutClause(String,Option<SortDirection>,Option<OffsetLimit>),
-        Get(String,Option<Clause>,Option<SortDirection>,Option<OffsetLimit>),
-        DeleteIndex(String,String),
-        DeleteWithoutClause(String),
-        Delete(String,Option<Clause>),
-        None
-    }
+#[derive(PartialEq, Debug, Clone)]
+pub enum Dql {
+    Create(String, Value),
+    Open(String),
+    Drop(String),
+    Len(String),
+    Upsert(String,Value,Option<Clause>),
+    Put(String,String,Value),
+    Exists(String,String),
+    Search(String,String,Option<SortDirection>,Option<OffsetLimit>),
+    GetIndex(String,String),
+    GetWithoutClause(String,Option<SortDirection>,Option<OffsetLimit>),
+    Get(String,Option<Clause>,Option<SortDirection>,Option<OffsetLimit>),
+    DeleteIndex(String,String),
+    DeleteWithoutClause(String),
+    Delete(String,Option<Clause>),
+    None
+}
 
-    fn recur(pair: Pair<Rule>) -> Dql {
-        let ret = match pair.as_rule() {
-            Rule::expr => recur(pair.into_inner().next().unwrap()),
-            Rule::create => {
-                let kv = kv(pair);
-                Dql::Create(
-                    kv.0,
-                    serde_json::from_str(
-                        &kv.1
-                    ).unwrap()
-                )
-            }
-            Rule::drop => {
-                Dql::Drop(
-                    k(pair)
-                )
-            }
-            Rule::len => {
-                Dql::Len(
-                    k(pair)
-                )
-            }
-            Rule::upsert => {
-                let kv = kv(pair);
-                Dql::Upsert(
-                    kv.0,
-                    serde_json::from_str(
-                        &kv.1
-                    ).unwrap(),
-                    None
-                )
-            }
-            Rule::upsert_where => {
-                let mut p = pair.clone().into_inner();
-                let col = k(pair);
+fn recur(pair: Pair<Rule>) -> Dql {
+    let ret = match pair.as_rule() {
+        Rule::expr => recur(pair.into_inner().next().unwrap()),
+        Rule::create => {
+            let kv = kv(pair);
+            Dql::Create(
+                kv.0,
+                serde_json::from_str(
+                    &kv.1
+                ).unwrap()
+            )
+        }
+        Rule::drop => {
+            Dql::Drop(
+                k(pair)
+            )
+        }
+        Rule::len => {
+            Dql::Len(
+                k(pair)
+            )
+        }
+        Rule::upsert => {
+            let kv = kv(pair);
+            Dql::Upsert(
+                kv.0,
+                serde_json::from_str(
+                    &kv.1
+                ).unwrap(),
+                None
+            )
+        }
+        Rule::upsert_where => {
+            let mut p = pair.clone().into_inner();
+            let col = k(pair);
 
+            p.next();
+
+            let lh_pair = p.next().unwrap();
+            let doc: Value = serde_json::from_str(
+                &str(lh_pair)
+            ).unwrap();
+
+            let mut clause = Clause::default();
+            cl(&mut clause, p);
+
+            Dql::Upsert(col, doc, Some(clause))
+        }
+        Rule::put => {
+            let kv = kv(pair.clone());
+            let mut p = pair.into_inner();
+            p.next();
+            p.next();
+            let obj = str(p.next().unwrap());
+            Dql::Put(
+                kv.0,
+                kv.1,
+                serde_json::from_str(&obj.as_str()).unwrap()
+            )
+        }
+        Rule::exi => {
+            let kv = kv(pair);
+            Dql::Exists(kv.0, kv.1)
+        }
+        Rule::search => {
+            let kv = kv(pair.clone());
+            let p = pair.into_inner();
+            Dql::Search(kv.0, kv.1,sr(p.clone()),ol(p))
+        }
+        Rule::select => {
+            let mut p = pair.clone().into_inner();
+            let col = p.next().unwrap().as_str().to_string();
+            let empty = p.next().unwrap().as_str().to_string();
+            if empty.eq("{}") {
+                let sort = sr(p.clone());
+                let ofsl = ol(p.clone());
+
+                Dql::GetWithoutClause(col, sort,ofsl)
+            } else {
+                p = pair.into_inner();
                 p.next();
 
-                let lh_pair = p.next().unwrap();
-                let doc: Value = serde_json::from_str(
-                    &str(lh_pair)
-                ).unwrap();
+                let mut clause = Clause::default();
+                cl(&mut clause, p.clone());
+
+                let sort = sr(p.clone());
+                let ofsl = ol(p.clone());
+
+                Dql::Get(col,  Some(clause),sort,ofsl)
+            }
+        }
+        Rule::get => {
+            let kv = kv(pair);
+            Dql::GetIndex(kv.0, kv.1)
+        }
+        Rule::delete => {
+            let kv = kv(pair);
+            Dql::DeleteIndex(kv.0, kv.1)
+        }
+        Rule::delete_where => {
+            let mut p = pair.clone().into_inner();
+            let col = p.next().unwrap().as_str().to_string();
+            let empty = p.next().unwrap().as_str().to_string();
+            if empty.eq("{}") {
+                Dql::DeleteWithoutClause(col)
+            } else {
+                p = pair.into_inner();
+                p.next();
 
                 let mut clause = Clause::default();
                 cl(&mut clause, p);
 
-                Dql::Upsert(col, doc, Some(clause))
-            }
-            Rule::put => {
-                let kv = kv(pair.clone());
-                let mut p = pair.into_inner();
-                p.next();
-                p.next();
-                let obj = str(p.next().unwrap());
-                Dql::Put(
-                    kv.0,
-                    kv.1,
-                    serde_json::from_str(&obj.as_str()).unwrap()
-                )
-            }
-            Rule::exi => {
-                let kv = kv(pair);
-                Dql::Exists(kv.0, kv.1)
-            }
-            Rule::search => {
-                let kv = kv(pair.clone());
-                let p = pair.into_inner();
-                Dql::Search(kv.0, kv.1,sr(p.clone()),ol(p))
-            }
-            Rule::select => {
-                let mut p = pair.clone().into_inner();
-                let col = p.next().unwrap().as_str().to_string();
-                let empty = p.next().unwrap().as_str().to_string();
-                if empty.eq("{}") {
-                    let sort = sr(p.clone());
-                    let ofsl = ol(p.clone());
-
-                    Dql::GetWithoutClause(col, sort,ofsl)
-                } else {
-                    p = pair.into_inner();
-                    p.next();
-
-                    let mut clause = Clause::default();
-                    cl(&mut clause, p.clone());
-
-                    let sort = sr(p.clone());
-                    let ofsl = ol(p.clone());
-
-                    Dql::Get(col,  Some(clause),sort,ofsl)
-                }
-            }
-            Rule::get => {
-                let kv = kv(pair);
-                Dql::GetIndex(kv.0, kv.1)
-            }
-            Rule::delete => {
-                let kv = kv(pair);
-                Dql::DeleteIndex(kv.0, kv.1)
-            }
-            Rule::delete_where => {
-                let mut p = pair.clone().into_inner();
-                let col = p.next().unwrap().as_str().to_string();
-                let empty = p.next().unwrap().as_str().to_string();
-                if empty.eq("{}") {
-                    Dql::DeleteWithoutClause(col)
-                } else {
-                    p = pair.into_inner();
-                    p.next();
-
-                    let mut clause = Clause::default();
-                    cl(&mut clause, p);
-
-                    Dql::Delete(col, Some(clause))
-                }
-            }
-            _=> Dql::None
-        };
-        ret
-    }
-    fn ol(mut p: Pairs<Rule>) -> Option<OffsetLimit> {
-        let mut offset: Option<u64> = None;
-        let mut limit: Option<u64> = None;
-        loop {
-            let next = p.next();
-            if next.is_none() {
-                break;
-            } else {
-                let next = next.unwrap();
-                match next.as_rule() {
-                    Rule::offset => {
-                        let ofs = next.as_str().to_uppercase()
-                            .replace("OFFSET","")
-                            .trim()
-                            .parse::<u64>();
-                        if ofs.is_ok() {
-                            offset = Some(ofs.unwrap());
-                        }
-                    }
-                    Rule::limit => {
-                        let lmt = next.as_str().to_uppercase()
-                            .replace("LIMIT","")
-                            .trim()
-                            .parse::<u64>();
-                        if lmt.is_ok() {
-                            limit = Some(lmt.unwrap());
-                        }
-                    }
-                    _=>{}
-                }
+                Dql::Delete(col, Some(clause))
             }
         }
-        if offset.is_none() && limit.is_none() {
-            None
+        _=> Dql::None
+    };
+    ret
+}
+fn ol(mut p: Pairs<Rule>) -> Option<OffsetLimit> {
+    let mut offset: Option<u64> = None;
+    let mut limit: Option<u64> = None;
+    loop {
+        let next = p.next();
+        if next.is_none() {
+            break;
         } else {
-            Some(OffsetLimit {
-                offset,
-                limit,
-            })
-        }
-    }
-    fn sr(mut p: Pairs<Rule>) -> Option<SortDirection> {
-        let mut field: Option<String> = None;
-        let mut direction: Option<Direction> = None;
-        loop {
-            let next = p.next();
-            if next.is_none() {
-                break;
-            } else {
-                let next = next.unwrap();
-                match next.as_rule() {
-                    Rule::sort => {
-                        field = Some(next.as_str()
-                            .replace("SORT","")
-                            .replace("sort","")
-                            .trim().to_string());
+            let next = next.unwrap();
+            match next.as_rule() {
+                Rule::offset => {
+                    let ofs = next.as_str().to_uppercase()
+                        .replace("OFFSET","")
+                        .trim()
+                        .parse::<u64>();
+                    if ofs.is_ok() {
+                        offset = Some(ofs.unwrap());
                     }
-                    Rule::direction => {
-                        let kwr = next.as_str().trim().to_uppercase();
-                        direction = Some(
-                            match kwr.as_str() {
-                                "ASC"=> Direction::Asc,
-                                "DESC"=> Direction::Desc,
-                                _=> Direction::Asc
-                            }
-                        );
-                    }
-                    _=>{}
                 }
+                Rule::limit => {
+                    let lmt = next.as_str().to_uppercase()
+                        .replace("LIMIT","")
+                        .trim()
+                        .parse::<u64>();
+                    if lmt.is_ok() {
+                        limit = Some(lmt.unwrap());
+                    }
+                }
+                _=>{}
             }
         }
-        if field.is_none() && direction.is_none() {
-            None
+    }
+    if offset.is_none() && limit.is_none() {
+        None
+    } else {
+        Some(OffsetLimit {
+            offset,
+            limit,
+        })
+    }
+}
+fn sr(mut p: Pairs<Rule>) -> Option<SortDirection> {
+    let mut field: Option<String> = None;
+    let mut direction: Option<Direction> = None;
+    loop {
+        let next = p.next();
+        if next.is_none() {
+            break;
         } else {
-            Some(SortDirection{ field, direction })
-        }
-    }
-    fn cl(clause: &mut Clause, mut p: Pairs<Rule>) {
-        let mut conjunction = format!("");
-        loop {
-            let next = p.next();
-            if next.is_none() {
-                break;
-            } else {
-                let next = next.unwrap().clone();
-                match next.as_rule() {
-                    Rule::conjunction => {
-                        conjunction = str(next);
-                        match conjunction.as_str() {
-                            "$or" => {
-                                clause.or = Some(Value::Array(vec![]));
-                            },
-                            "$and" => {
-                                clause.and = Some(Value::Array(vec![]));
-                            },
-                            _=> {}
-                        }
-                    }
-                    Rule::clause_array => {
-                        let v: Option<Value> = Some(serde_json::from_str(&str(next).as_str()).unwrap());
-                        match conjunction.as_str() {
-                            "$or" => {
-                                clause.or = v;
-                            },
-                            "$and" => {
-                                clause.and = v;
-                            },
-                            _=> {
-                                println!("what is it ? {}",conjunction);
-                            }
-                        }
-                    }
-                    _ => {}
+            let next = next.unwrap();
+            match next.as_rule() {
+                Rule::sort => {
+                    field = Some(next.as_str()
+                        .replace("SORT","")
+                        .replace("sort","")
+                        .trim().to_string());
                 }
+                Rule::direction => {
+                    let kwr = next.as_str().trim().to_uppercase();
+                    direction = Some(
+                        match kwr.as_str() {
+                            "ASC"=> Direction::Asc,
+                            "DESC"=> Direction::Desc,
+                            _=> Direction::Asc
+                        }
+                    );
+                }
+                _=>{}
             }
         }
     }
-    fn k(opt: Pair<Rule>) -> String {
-        let mut pair = opt.into_inner();
-        str(pair.next().unwrap())
+    if field.is_none() && direction.is_none() {
+        None
+    } else {
+        Some(SortDirection{ field, direction })
     }
-    fn kv(opt: Pair<Rule>) -> (String, String) {
-        let mut pair = opt.into_inner();
-        let index_id = str(pair.next().unwrap());
-        let doc = str(pair.next().unwrap());
-        (index_id, doc)
+}
+fn cl(clause: &mut Clause, mut p: Pairs<Rule>) {
+    let mut conjunction = format!("");
+    loop {
+        let next = p.next();
+        if next.is_none() {
+            break;
+        } else {
+            let next = next.unwrap().clone();
+            match next.as_rule() {
+                Rule::conjunction => {
+                    conjunction = str(next);
+                    match conjunction.as_str() {
+                        "$or" => {
+                            clause.or = Some(Value::Array(vec![]));
+                        },
+                        "$and" => {
+                            clause.and = Some(Value::Array(vec![]));
+                        },
+                        _=> {}
+                    }
+                }
+                Rule::clause_array => {
+                    let v: Option<Value> = Some(serde_json::from_str(&str(next).as_str()).unwrap());
+                    match conjunction.as_str() {
+                        "$or" => {
+                            clause.or = v;
+                        },
+                        "$and" => {
+                            clause.and = v;
+                        },
+                        _=> {
+                            println!("what is it ? {}",conjunction);
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
     }
-    fn str(opt: Pair<Rule>) -> String {
-        opt.as_str().to_string()
-    }
+}
+fn k(opt: Pair<Rule>) -> String {
+    let mut pair = opt.into_inner();
+    str(pair.next().unwrap())
+}
+fn kv(opt: Pair<Rule>) -> (String, String) {
+    let mut pair = opt.into_inner();
+    let index_id = str(pair.next().unwrap());
+    let doc = str(pair.next().unwrap());
+    (index_id, doc)
+}
+fn str(opt: Pair<Rule>) -> String {
+    opt.as_str().to_string()
+}
 
-    pub fn parse(dql: &str) -> Result<Vec<Dql>, Error> {
-        let pairs = DqlParser::parse(Rule::program,dql);
-        if pairs.is_err() {
-            Err(anyhow!("{}",pairs.err().unwrap()))
-        } else {
-            let mut nodes = vec![];
-            let pairs = pairs.unwrap();
-            for pair in pairs {
-                match pair.as_rule() {
-                    Rule::expr => nodes.push(recur(pair)),
-                    _=>{}
-                }
+pub fn parse(dql: &str) -> Result<Vec<Dql>, Error> {
+    let pairs = DqlParser::parse(Rule::program,dql);
+    if pairs.is_err() {
+        Err(anyhow!("{}",pairs.err().unwrap()))
+    } else {
+        let mut nodes = vec![];
+        let pairs = pairs.unwrap();
+        for pair in pairs {
+            match pair.as_rule() {
+                Rule::expr => nodes.push(recur(pair)),
+                _=>{}
             }
-            Ok(nodes)
         }
+        Ok(nodes)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use std::time::Instant;
-    use crate::flinch_dql::Dql;
     use super::*;
 
     #[test]
